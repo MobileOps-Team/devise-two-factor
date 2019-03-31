e78e2f4ddbad58a83b0922c67205551c0d4f06ae
Michael Armfield <michael@mobileops.co>
2019-03-31 13:51:40 -0700

    Test

--- lib/devise_two_factor/models/two_factor_authenticatable.rb
+++ lib/devise_two_factor/models/two_factor_authenticatable.rb
@@ -15,8 +15,7 @@ module Devise
         unless attr_encrypted?(:otp_secret)
           attr_encrypted :otp_secret,
             :key  => self.otp_secret_encryption_key,
-            :encode => true,
-            :mode => :per_attribute_iv_and_salt unless self.attr_encrypted?(:otp_secret)
+            :encode => true
         end
 
         attr_accessor :otp_attempt
